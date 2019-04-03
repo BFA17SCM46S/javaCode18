eb360b2d0eab8ee8d776c4bfa7008feab88f729c
BFA17SCM46S <33650298+BFA17SCM46S@users.noreply.github.com>
2019-04-03 13:57:50 -0500

    Create Snake.java

---
+++ Snake.java
@@ -0,0 +1,35 @@
+package basics;
+
+public class Snake {
+	
+	/**
+	 * move at the give speed 
+	 * @param speed
+	 */
+	public void move(int speed) {}
+
+	/**
+	 * move given direction  1 west 2 east 3 south 4 north
+	 * @param speed
+	 */
+	public void turn(int direction) {}
+
+	/**
+	 * snake eat food and grows
+	 * @param speed
+	 */
+	public void eat() {}
+
+	/**
+	 * snake increase the size after eating
+	 * @param speed
+	 */
+	public void grow() {}
+	
+	void die(){};
+
+	
+	void shrink(){};
+	
+
+}
